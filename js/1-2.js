8c9c0edb390f264b4a30359ae1a3cd1e73fdcd9b
Hectorlag <Hectorvlaguna@gmail.com>
2023-05-19 17:08:40 -0300

    Pagina calculadora

---
+++ js/1-2.js
@@ -0,0 +1,18 @@
+
+function insert(num) {
+    var value = $('.display').val();
+    $('.display').val(value + num);
+}
+
+function c(){
+    $('.display').val('');
+}
+
+function del(){
+    var value = $('.display').val();
+    $('.display').val(value.substring(0,value.length-1));
+}
+
+function igual(){
+    $('.display').val(eval($('.display').val()));
+}
